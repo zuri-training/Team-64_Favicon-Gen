60ef8760fe37c6b6a4e340df653b48db9919993c
WakHerv7 <wakam.hermann@gmail.com>
2022-08-01 20:23:54 +0100

    wakherv-20220801-generator_page_responsive_tablet_and_mobile_Complete

--- GeneratorPage/js_hw/range_slider.js
+++ GeneratorPage/js_hw/range_slider.js
@@ -1,6 +1,8 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("range_output_hw");
-output.innerHTML = slider.value;
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("range_output_hw");
+
+// output.innerHTML = slider.value;
+
+// slider.oninput = function() {
+//   output.innerHTML = this.value;
+// }
