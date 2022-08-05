ecc4b40317484f1de830d2b56c604cc12c0cf9a1
jolomi212 <65557108+jolomi212@users.noreply.github.com>
2022-08-05 19:59:44 +0100

    Delete GeneratorPage directory

--- GeneratorPage/js_hw/range_slider.js
+++
@@ -1,8 +0,0 @@
-// var slider = document.getElementById("myRange");
-// var output = document.getElementById("range_output_hw");
-
-// output.innerHTML = slider.value;
-
-// slider.oninput = function() {
-//   output.innerHTML = this.value;
-// }
