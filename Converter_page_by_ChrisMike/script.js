cbd1e59dd4c478fc1c14775a242822daf5c0a0c3
ChrisMike10 <awoyemimike5@gmail.com>
2022-08-06 16:26:07 +0100

    media responsiveness and changes to the navbar

--- Converter_page_by_ChrisMike/script.js
+++ Converter_page_by_ChrisMike/script.js
@@ -1,3 +1,27 @@
+//Navbar Active State
+document.querySelectorAll('.nav-link').forEach(link => {
+    if(link.href === window.location.href) {
+        link.setAttribute('aria-current', 'page')
+    }
+})
+
+//Hamburger-Menu nav-link Active State 
+document.querySelectorAll('.mobile-nav-link').forEach(link => {
+    if(link.href === window.location.href) {
+        link.setAttribute('aria-current', 'page')
+    }
+}) 
+
+//Hamburger-Menu Open & Close
+const menu = document.querySelector('.menu-icon')
+const mobile = document.querySelector('.mobile-menu')
+const nav = document.querySelector('.navbar_ma')
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active')
+  mobile.classList.toggle('active')
+  nav.classList.toggle('no-scroll')
+})
 
 // Drag and Drop function
 
@@ -22,7 +46,7 @@ const initApp = () => {
         droparea.addEventListener(evtName, inactive);
     });
 
-    droparea.addEventListener("drop", handleDrop);
+    droparea.addEventListener('drop', handleDrop);
 
 }
 
@@ -32,16 +56,25 @@ const handleDrop = (e) => {
     const dt = e.dataTransfer;
     const files = dt.files;
     const fileArray = [...files];
-    console.log(files); // FileList
-    console.log(fileArray);
-} 
+}
 
-
-//Upload Image function
+//Upload Image and Browse function
 
 const realFileBtn = document.getElementById("real-file");
+const browseLink = document.getElementById("browse");
 const customBtn = document.getElementById("custom-button");
 
 customBtn.addEventListener("click", function() {
     realFileBtn.click()
+})
+
+browseLink.addEventListener("click", function() {
+    realFileBtn.click()
+
+})
+
+//Upload Image button bgcolor change 
+const btn = document.getElementById('custom-button');
+btn.addEventListener('click', function onClick(event) {
+    event.target.style.backgroundColor = "var(--blue)" ;
 })
