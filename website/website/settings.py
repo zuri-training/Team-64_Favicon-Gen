b66f78cc1e19c6be963e35c9a58c8ca598d6883a
Ridwan Adeyemo <adesolaridwan2003@gmail.com>
2022-08-14 00:42:49 -0400

    removed STATICFILES_DIRS

--- website/website/settings.py
+++ website/website/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS= [
     # START:for_download_tuto
-    os.path.join(BASE_DIR,'static'),
+    # os.path.join(BASE_DIR,'static'),
     # END:for_download_tuto
     os.path.join(BASE_DIR,'assets')]
 
