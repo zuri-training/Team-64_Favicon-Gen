82913aa1bc4022aa8d20d29a001ce9781f70b64e
WakHerv7 <wakam.hermann@gmail.com>
2022-08-17 00:31:14 +0100

    wakherv-20220816-remove-Keys

--- website/website/settings.py
+++ website/website/settings.py
@@ -156,10 +156,4 @@ LOGOUT_REDIRECT_URL = '/sign-in'
 
 
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey' #keep this as it is
-EMAIL_HOST_PASSWORD = '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' 
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'zuricongen@gmail.com' 
+
